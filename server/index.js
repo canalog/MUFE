d572e64e2dde11d5e422ca1c6f7342e39f2d3cba
canalog <alicecha719@naver.com>
2022-01-22 04:20:46 +0900

    client structure

--- server/index.js
+++ server/index.js
@@ -17,12 +17,13 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", (req, res) => {
-  res.send("Thank");
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요");
 });
 
 app.post("/api/users/register", (req, res) => {
@@ -85,5 +86,5 @@ app.get("/api/users/logout", auth, (req, res) => {
 });
 const port = 3005;
 app.listen(port, () => {
-  console.log("Server started on port 3000");
+  console.log(`Server started on port ${port}`);
 });
